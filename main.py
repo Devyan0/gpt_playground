ad347abdb1c31981433f4d903127f654fe692d10
Seunghyun Yang <haeundae96@gmail.com>
2023-03-18 08:56:00 +0900

    test: api payment, finding free trials

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import json
+import openai
 
 # Load the JSON file
-with open('api_key.json') as f:
-    data = json.load(f)
+try:
+    with open('api_key.json') as f:
+        data = json.load(f)
+    openai.api_key = data['OPENAI_API_KEY']
+except:
+    print('Need to create api_key.json file with OPENAI_API_KEY')
+    exit(1)
 
-# Extract the API key
-api_key = data['OPENAI_API_KEY']
+# list models
+models = openai.Model.list()
 
-print(api_key)
+# https://help.openai.com/en/articles/7039783-chatgpt-api-faq
+
+# print the first model's id
+mod = models.data[0].id
+
+# create a completion
+completion = openai.Completion.create(model=mod, prompt="Hello world")
+
+# print the completion
+print(completion.choices[0].text)
+
